fed20c9205cc980fe98ba3d1f2ff739d7f4c471f
musiclvme <wtw2000@126.com>
2021-05-21 13:54:19 +0800

    add subplot for more pictures
    Change-Id: Ied29674e6878f7777b401b333c8a0a060c732083

--- test/01.hello.py
+++ test/01.hello.py
@@ -14,14 +14,32 @@ n = round(duration * samplerates)
 peroid = duration / freq     # 1 peroid time
 ts = np.arange(0,peroid,peroid/200)
 phases = PI2 * freq * ts + offset  # 2*PI*ft + x
+phases1 = PI2 * freq * ts + offset + 0.5  # 2*PI*ft + x
 y = amp * np.sin(phases)
+y1 = amp * np.sin(phases1)
 
 
 # draw your audio waves
+# setting size for your picture
+fig = plt.figure(figsize=(10, 10))
 
+# total 2*1 = 2 sub picture
+# pic1 is the first one
+# pic2 is the second one
+pic1 = fig.add_subplot(2,1,1)
+pic2 = fig.add_subplot(2,1,2)
 
-plt.plot(ts,y)
+# pic1
+pic1.scatter(ts*500, y, color='red')
+pic1.set_xlabel('time*500')
+pic1.set_ylabel('vol')
+
+# pic2
+pic2.scatter(ts*500,y1, color='g', linestyle='--')
+
 plt.show()
+
+plt.close()
 
 
 
